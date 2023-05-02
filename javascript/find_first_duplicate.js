ec709a91e35a02b400e3b6d3c80702bc4563c846
Historia Montague <historia.montague@outlook.com>
2023-05-02 12:43:28 -0600

    Finished Lab

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,12 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let dupIndex = -1
+  for(let i = 0; i === dupIndex || i < arr.length; i++) { 
+    const tempIndex = arr.slice(i + 1, arr.length).findIndex(num => num === arr[i])
+    dupIndex === -1?dupIndex = tempIndex:true 
+  }
+  debugger
+  return dupIndex===-1?dupIndex:arr[dupIndex]
 }
 
 if (require.main === module) {
@@ -16,4 +23,11 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+//Set a default dupplicate index of -1
+//Iterate through the array and check if a value exists elsewhere in the array
+//keep the lowest value
+//return dup index
 // And a written explanation of your solution
+//Sets a default value for dupIndex to -1
+//Iterates through the array for the whole length or as long as i, the current index, is lower than the dup index
+//returns dupIndex
